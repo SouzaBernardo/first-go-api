4c74ae15f33329401aa922ea45d7d4e76ad2495b
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2024-01-16 17:14:15 -0300

    bootstrap and import index on main.go

--- main.go
+++ main.go
@@ -5,6 +5,13 @@ import (
 	"net/http"
 )
 
+type Product struct {
+	Name        string
+	Description string
+	Price       float64
+	Quantity    int
+}
+
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
@@ -13,5 +20,12 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	temp.ExecuteTemplate(w, "Index", nil)
+
+	products := []Product{
+		{Name: "Shoe", Description: "Blue", Price: 25, Quantity: 5},
+		{Name: "Tshirt", Description: "Purple", Price: 30, Quantity: 2},
+		{Name: "Socke", Description: "White", Price: 15, Quantity: 2},
+	}
+
+	temp.ExecuteTemplate(w, "Index", products)
 }
