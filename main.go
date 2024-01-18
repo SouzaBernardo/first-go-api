ac67e30ec47f13c6b0809135ffd2896a2f2d704b
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2024-01-18 14:46:15 -0300

    refact: using modules in main

--- main.go
+++ main.go
@@ -1,73 +1,14 @@
 package main
 
 import (
-	"database/sql"
-	"html/template"
+	"SouzaBernardo/first-go-api/routes"
 	"net/http"
 
 	_ "github.com/lib/pq"
 )
 
-type Product struct {
-	Id 			int
-	Name        string
-	Description string
-	Price       float64
-	Quantity    int
-}
-
-var temp = template.Must(template.ParseGlob("templates/*.html"))
-
 func main() {
 
-	http.HandleFunc("/", index)
+	routes.CreateRoutes()
 	http.ListenAndServe(":8080", nil)
-}
-
-func index(w http.ResponseWriter, r *http.Request) {
-
-	db := connectDB()
-
-	allProduct, err := db.Query("select * from product;")
-	
-	if err != nil {
-		panic(err.Error())
-	}
-
-	p := Product{}
-	products := []Product{}
-	
-	for allProduct.Next() {
-		var id, quantity int
-		var name, description string
-		var price float64
-
-		err = allProduct.Scan(&id, &name, &description, & price, &quantity)
-		
-		if err != nil {
-			panic(err.Error())
-		}
-		p.Id = id
-		p.Name = name
-		p.Description = description
-		p.Price = price
-		p.Quantity = quantity
-
-		products = append(products, p)
-
-	}
-
-	temp.ExecuteTemplate(w, "Index", products)
-	defer db.Close()
-}
-
-func connectDB() *sql.DB {
-	connection := "user=postgres dbname=store password=postgres host=localhost sslmode=disable"
-	db, err := sql.Open("postgres", connection)
-
-	if err != nil {
-		panic(err.Error())
-	}
-
-	return db
 }
