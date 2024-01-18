bd2aa16e1f5d8a16ce9dfd8580af98e4fa6d30df
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2024-01-18 14:45:48 -0300

    db schema

---
+++ scripts/schema.sql
@@ -0,0 +1,15 @@
+create table if not exists  product (
+	id serial primary key,
+	name varchar(255) not null,
+	description varchar(255),
+	price decimal,
+	quantity integer
+);
+
+insert into product(name, description, price, quantity)
+	values('Shoe', 'Blue', 10, 2);
+	
+insert into product(name, description, price, quantity)
+	values('Tshirt', 'White', 8, 1);
+	
+select * from product;
