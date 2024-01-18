a05afa5e2e9e1f11f738817b8938205cc864f09b
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2024-01-18 14:43:18 -0300

    feat: connectDB function

---
+++ database/db.go
@@ -0,0 +1,18 @@
+package database
+
+import (
+	"database/sql"
+
+	_ "github.com/lib/pq"
+)
+
+func ConnectDB() *sql.DB {
+	connection := "user=postgres dbname=store password=postgres host=localhost sslmode=disable"
+	db, err := sql.Open("postgres", connection)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return db
+}
