1b420542e8585a4c883c5e95c957b3110bbb9734
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2024-01-22 08:31:38 -0300

    move: schema.sql

--- model/scripts/schema.sql
+++ model/scripts/schema.sql
@@ -1,3 +1,11 @@
+CREATE DATABASE if not exists store
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LOCALE_PROVIDER = 'libc'
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
 create table if not exists  product (
 	id serial primary key,
 	name varchar(255) not null,
@@ -5,11 +13,7 @@ create table if not exists  product (
 	price decimal,
 	quantity integer
 );
-
 insert into product(name, description, price, quantity)
 	values('Shoe', 'Blue', 10, 2);
-	
 insert into product(name, description, price, quantity)
 	values('Tshirt', 'White', 8, 1);
-	
-select * from product;
