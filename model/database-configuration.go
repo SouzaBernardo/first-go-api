f40693fdd3e9e7f73194c71e25ec927e056b7323
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2024-01-26 09:56:13 -0300

    feat: add envs

--- model/database-configuration.go
+++ model/database-configuration.go
@@ -4,14 +4,14 @@ import (
 	"database/sql"
 	"fmt"
 	_ "github.com/lib/pq"
+	"os"
 )
 
 func ConnectDB() *sql.DB {
-	user := "postgres"
-	password := "postgres"
-	dbname := "postgres"
-	host := "localhost"
-	//host := "postgres-container"
+	user := os.Getenv("POSTGRES_USER")
+	password := os.Getenv("POSTGRES_PASSWORD")
+	dbname := os.Getenv("POSTGRES_DB")
+	host := os.Getenv("POSTGRES_HOST")
 	connection := fmt.Sprintf("user=%s dbname=%s password=%s host=%s sslmode=disable", user, dbname, password, host)
 	open, err := sql.Open("postgres", connection)
 
