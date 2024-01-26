98041971f0596488e3c0f07eebb33b97d5e12290
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2024-01-25 21:27:02 -0300

    refact: transaction and add on default data base

--- model/scripts/schema.sql
+++ model/scripts/schema.sql
@@ -1,6 +1,8 @@
-CREATE DATABASE store;
+-- CREATE DATABASE store;
+-- \connect store;
 
-\connect store;
+
+begin;
 
 create table if not exists product (
 	id serial primary key,
@@ -9,8 +11,10 @@ create table if not exists product (
 	price decimal,
 	quantity integer
 );
-insert into product(name, description, price, quantity)
-	values('Shoe', 'Blue', 10, 2);
 
 insert into product(name, description, price, quantity)
+	values('Shoe', 'Blue', 10, 2);
+insert into product(name, description, price, quantity)
 	values('Tshirt', 'White', 8, 1);
+
+commit;
